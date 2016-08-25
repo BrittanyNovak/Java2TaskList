544e43a703c932435d129d1adb796286fb88cccf
Brittany Novak <brittanynicole0106@gmail.com>
2016-08-24 19:40:55 -0500

    unbreaking shit

--- app/src/main/java/com/example/student/java2tasklist/DBHelper.java
+++ app/src/main/java/com/example/student/java2tasklist/DBHelper.java
@@ -6,8 +6,10 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
@@ -18,15 +20,17 @@ import java.util.ArrayList;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "MyDBName.db";
+    public static final String DATABASE_NAME = "tasklists.db";
     public static final String TASK_LIST_NAME = "TASKS";
     public static final String TASKS_COLUMN_TASK = "TASK";
     public static final String TASKS_COLUMN_IS_COMPLETED = "IS_COMPLETED";
     private static String DB_PATH = "/data/data/com.example.student.java2tasklist/databases/";
     private final Context myContext;
+    private SQLiteDatabase myDataBase;
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
+        this.myContext = context;
     }
 
     @Override
@@ -37,26 +41,31 @@ public class DBHelper extends SQLiteOpenHelper {
                // "IS_COMPLETED," +
                // "ENTERED DATETIME DEFAULT CURRENT_TIMESTAMP)" );
 
-        InputStream myInput = myContext.getAssets().open(DATABASE_NAME);
-        String outFileName = DB_PATH + DATABASE_NAME;
-        OutputStream myOutput = new FileOutputStream(outFileName);
+        try {
 
-        byte[] buffer = new byte[1024];
-        int length;
-        while((length = myInput.read(buffer))>0){
-            myOutput.write(buffer, 0, length);
+            InputStream myInput = myContext.getAssets().open(DATABASE_NAME);
+            String outFileName = DB_PATH + DATABASE_NAME;
+            OutputStream myOutput = new FileOutputStream(outFileName);
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = myInput.read(buffer)) > 0) {
+                myOutput.write(buffer, 0, length);
+            }
+
+            myOutput.flush();
+            myOutput.close();
+            myInput.close();
+        } catch (IOException e){
+            // Failed to write to database
+            Toast.makeText(myContext, "Unable to write db", Toast.LENGTH_SHORT).show();
         }
-
-        myOutput.flush();
-        myOutput.close();
-        myInput.close();
-
     }
 
     public void openDatabase() throws SQLException {
 
         String myPath = DB_PATH + DATABASE_NAME;
-        myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase);
+        myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
     }
 
     @Override
